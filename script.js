6d047f1f253b5d8dedd967da4a1add80310d2172
nyarlixaskk-ui <nyarlixaskk@gmail.com>
2026-03-20 18:46:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// simple scroll animation
+const elements = document.querySelectorAll('.card');
 
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    const pos = el.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 50) {
+      el.style.opacity = 1;
+      el.style.transform = "translateY(0)";
+    }
+  });
+});
